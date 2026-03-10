927bc482b4652afd7b2f4ad8c097697913599f39
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-03-10 21:06:45 +0600

    Add functional component 'Function'
    Created a functional component named 'Function' that returns an empty div.

--- Function.jsx
+++ Function.jsx
@@ -1,1 +1,12 @@
+================Funtional Components/Array Function=======================
+const Function = () => {
+    return (
+        <div>
+          
+        </div>
+    );
+};
 
+export default Function;
+
+-----------ekhane Function Hocce akta component--------------------
