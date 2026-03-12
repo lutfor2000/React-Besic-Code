d5cc6d739c926374bdce3412f23b243996961a34
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-03-12 22:02:58 +0600

    Update Loop.jsx

--- Loop.jsx
+++ Loop.jsx
@@ -1,1 +1,19 @@
 
+import React from 'react';
+const city = ['Dhaka','Rangpur','Pakhisthan','Nepal'];
+
+const Loop = () => {
+    return (
+        <div>
+            <ul>
+                {
+                  city.map((item,i)=>{
+                      return <li key={i.toString()}>{i}-{item}</li>
+                  })
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default Loop;
