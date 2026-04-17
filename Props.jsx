7f28c355d03a91e63fa12e5ebbc3e997e6499f2f
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-04-17 18:00:43 +0600

    Update Props.jsx

--- Props.jsx
+++ Props.jsx
@@ -1,1 +1,28 @@
-//Props হলো parent component থেকে child component-এ data পাঠানোর একটি মাধ্যম।
+=>Props হলো parent component থেকে child component-এ data পাঠানোর একটি মাধ্যম।
+
+==========================Parent Component======================================
+import Hero from "./components/Hero"
+function App() {
+  return (
+    <div>
+      <Hero title="Lutfor Rhaman Shanto" age="24"/>
+    <div/>
+  )
+}
+
+export default App
+
+  
+
+==========================Child Component======================================
+const Hero = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <h1>{props.age}</h1>
+        </div>
+    );
+};
+
+export default Hero;
+
