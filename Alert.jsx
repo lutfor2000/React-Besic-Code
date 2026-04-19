4c850244effee7b1019faa5246cde3ed2411c2fc
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-04-19 17:33:02 +0600

    Update Alert.jsx

--- Alert.jsx
+++ Alert.jsx
@@ -1,1 +1,26 @@
+const Hero = () => {
 
+    function demo(){
+        alert("This is Demo Function");
+    }
+
+    return (
+        <div>
+            <button onClick={demo}>Submit Document</button>
+        </div>
+    );
+};
+
+export default Hero;
+
+=====================================Step 02===========================
+const Hero = () => {
+
+    return (
+        <div>
+            <button onClick={()=>{alert("Alert Success full")}}>Submit Document</button>
+        </div>
+    );
+};
+
+export default Hero;
