a6c66256940665b90e4ba3b0dbeb4547a93cf59c
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-04-19 18:20:46 +0600

    Update Form-Logic.jsx

--- Form-Logic.jsx
+++ Form-Logic.jsx
@@ -1,1 +1,18 @@
+const Hero = () => {
+    
+    const formData =(event)=>{
+        event.preventDefault();
+        // alert()
+    }
 
+    return (
+        <div>
+           <form onSubmit={formData}>
+              <input  placeholder="Enter Your Name"/>
+              <button type="submit">Send Message</button>
+           </form>
+        </div>
+    );
+};
+
+export default Hero;
