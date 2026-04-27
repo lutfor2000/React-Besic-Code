d99cde1543bc40aea0c62819eb4c91f7072effe6
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-04-27 22:33:37 +0600

    Update ApiCall.jsx

--- ApiCall.jsx
+++ ApiCall.jsx
@@ -1,1 +1,28 @@
+import { useRef } from "react";
 
+const Hero = () => {
+
+    let apiData = useRef(null);
+    let myPtag = useRef();
+
+    const fetchData = async ()=> {
+
+       const response = await fetch("https://dummyjson.com/products");
+       apiData.current = await response.json();
+    }
+    
+   
+    const showData = ()=>{
+        myPtag.current.innrText = JSON.stringify(apiData.current);
+    }
+
+    return(
+        <div>
+            <p ref={myPtag}></p>
+            <button onClick={fetchData}>Call API</button>
+            <button onClick={showData}>Show Data</button>
+        </div>
+    );
+};
+
+export default Hero;
