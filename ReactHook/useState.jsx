4a3fb09cd51f63231fca98b3a7501a048547ed56
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-04-28 15:09:18 +0600

    Update useState.jsx

--- ReactHook/useState.jsx
+++ ReactHook/useState.jsx
@@ -1,1 +1,14 @@
+import { useRef, useState } from "react";
 
+const Hero = () => {
+        const [number,setNumber] = useState(0);
+   
+    return(
+        <div>
+            <h1>Number: {number}</h1>
+            <button onClick={()=>setNumber(number+1)}>Click</button>
+        </div>
+    );
+};
+
+export default Hero;
