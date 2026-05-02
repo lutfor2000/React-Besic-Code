4564c02c8536d25d7d63d932309720b67165b2f9
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-05-02 22:23:01 +0600

    Update useEffice.jsx

--- ReactHook/useEffice.jsx
+++ ReactHook/useEffice.jsx
@@ -1,1 +1,8 @@
+🔹 useEffect() কী?
 
+React component render হওয়ার পর যেসব কাজ করতে হয় (API call, DOM update, timer, event listener), সেগুলো handle করার জন্য useEffect() ব্যবহার করা হয়।
+
+🔹 Basic Syntax
+useEffect(() => {
+  // side effect code
+}, []);
