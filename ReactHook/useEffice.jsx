240ee748f27d2bb77779810f097323c80a018c2c
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-05-03 18:43:11 +0600

    Update useEffice.jsx

--- ReactHook/useEffice.jsx
+++ ReactHook/useEffice.jsx
@@ -6,3 +6,56 @@ React component render হওয়ার পর যেসব কাজ করত�
 useEffect(() => {
   // side effect code
 }, []);
+
+//---------------Api Call use fetch-----------------
+import { useEffect, useRef, useState } from "react";
+
+const Hero = () =>{
+
+    let[data,setData] = useState();
+
+    useEffect(()=>{
+
+        fetch('https://dummyjson.com/products/categories')
+        .then(res => res.json())
+        .then(json => setData(json))
+
+    },[])
+
+    return(
+
+        <div>
+            <h1>{JSON.stringify(data)}</h1>
+        </div>
+    )
+}
+
+export default Hero;
+
+//---------------Api Call use async-----------------
+
+import { useEffect, useRef, useState } from "react";
+
+const Hero = () =>{
+
+    let[data,setData] = useState();
+
+    useEffect(()=>{
+
+        ( async ()=>{
+            let response = await fetch('https://dummyjson.com/products/category-list')
+            let result = await response.json();
+            setData(result);
+        })()
+
+    },[])
+
+    return(
+
+        <div>
+            <h1>{JSON.stringify(data)}</h1>
+        </div>
+    )
+}
+
+export default Hero;
