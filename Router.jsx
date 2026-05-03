e21b053fdbdd21a50091789930d7b944a61a4717
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-05-03 23:05:44 +0600

    Update Router.jsx

--- Router.jsx
+++ Router.jsx
@@ -1,4 +1,10 @@
+Link → Page change করে
+Route → কোন page show হবে decide করে 
 
+Link = যাওয়ার রাস্তা (Navigation)
+Route = গন্তব্য (Destination render)
+
+  
 ==> instal route cmd -> npm i react-router-dom
 
 import { BrowserRouter,Routes,Route } from "react-router-dom"
