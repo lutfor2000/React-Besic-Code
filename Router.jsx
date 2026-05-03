fc29dd5f3fb1aba08d6b7ceb626db9d9c066abc2
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-05-03 23:45:30 +0600

    Update Router.jsx

--- Router.jsx
+++ Router.jsx
@@ -29,3 +29,39 @@ function App() {
     </>
   )
 }
+
+
+//===============Routing Stape==============================
+
+ <BrowserRouter>
+     
+          <NavMenu/>
+
+          <Routes>
+
+               <Route path ="/images" element ={<Images/>} />
+               <Route path="/about" element = {<About/>} />
+               <Route path="/form" element = {<Form/>} />
+               <Route path="*" element={<NotFound/>}/>
+
+          </Routes>
+      </BrowserRouter>
+
+//=====================Page Link=================
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavMenu = () => {
+    return (
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/images">Images</Link></li>
+                <li><Link to="/form">Form</Link></li>
+            </ul>
+        </div>
+    );
+};
+
+export default NavMenu;
