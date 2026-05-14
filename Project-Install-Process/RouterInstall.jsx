32e93affd412cdf6b5da625f8fa0644477738274
Lutfor Rhaman <155230221+lutfor2000@users.noreply.github.com>
2026-05-14 22:45:07 +0600

    Update RouterInstall.jsx

--- Project-Install-Process/RouterInstall.jsx
+++ Project-Install-Process/RouterInstall.jsx
@@ -1,2 +1,37 @@
 ----------Install Proccess---------
 ->npm install react-router-dom
+
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+
+      {
+        path: "/",
+        element: <h1>Home</h1>
+      },
+      {
+        path: "/about",
+        element: <h1>About</h1>
+      },
+      {
+        path: "/contact",
+        element: <h1>Contact</h1>
+      },
+
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router} />
+  </StrictMode>,
+)
